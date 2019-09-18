42b36fd7122e6d8889e166894273c01a5be8329a
Peter Franzreb <pefra23@gmail.com>
2019-09-18 20:46:23 +0200

    Add http logic

--- main.go
+++ main.go
@@ -1,6 +1,28 @@
 package main
-import "fmt"
 
+import (
+    //"encoding/json"
+    "fmt"
+    "net/http"
+    "log"
+    "io/ioutil"
+    "os"
+)
 func main() {
-    fmt.Println("First test")
+	var ip string = os.Args[1]
+	var port string = "80"
+
+	var url string = "http://" + ip + ":" + port;
+	fmt.Println(url)
+	resp, err := http.Get(url + "/api")
+	if err != nil {
+		log.Fatal(err);
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(body))
+	//decode json and so on
+	defer resp.Body.Close()
 }
